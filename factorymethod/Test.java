6af666eb7fdd5143077f394deaadc9468d22e3a8
dongfeng2010ba <dongfeng2010@foxmail.com>
2019-03-04 21:28:05 +0800

    add factorymethod add beizhu

--- factorymethod/Test.java
+++ factorymethod/Test.java
@@ -3,6 +3,8 @@ package com.dong.factorymethod;
 public class Test {
     public static void main(String[] args) {
 
+
+        //这里具体是什么Factory可以是改成客户端输入，new了就可以用
         IFactory factory = new FactoryA();
         factory.creatProduct().Operation();
 
